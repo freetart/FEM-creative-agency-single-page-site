c8543b354eb3d4c8f3988b9811bcd238a7d8823d
habib <habibdev96@gmail.com>
2021-08-22 06:05:23 -0600

    created carousel section

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,13 @@
-"use scrict";
+import Glide from "@glidejs/glide";
+
+const carousel = () => {
+  const carouselConfig = {
+    type: "carousel",
+    autoplay: 12000,
+    perView: 1,
+    hoverpause: true,
+  };
+
+  new Glide(".glide", carouselConfig).mount();
+};
+carousel();
